3a52afbfe192e50760c96e114d2bf7c6028ff7ba
David Mello <theycallmedavidmello@gmail.com>
2017-04-20 22:19:21 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,65 @@
+var webdriver = require('selenium-webdriver'),
+    chrome = require('selenium-webdriver/chrome'),
+    fs = require('fs'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var config = require('./config.js');
+
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var d = new Date();
+var fileName = monthNames[d.getMonth()] + ' ' + d.getFullYear().toString().substr(-2) + ".txt";
+
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+driver.get(config.loginUrl);
+driver.findElement(By.id('email')).sendKeys(config.email);
+driver.findElement(By.id('field')).sendKeys(config.password);
+driver.findElement(By.css('h1')).click();
+driver.findElement(By.id('button')).click();
+driver.findElement(By.css("input[value='PayNow!']")).click();
+driver.findElement(By.name('header.accountNumber')).clear();
+driver.findElement(By.name('header.accountNumber')).sendKeys(config.accountNumber);
+driver.findElement(By.name('customer.address.zipCode')).clear();
+driver.findElement(By.name('customer.address.zipCode')).sendKeys(config.zipCode);
+driver.findElement(By.name('submitBtn')).click();
+driver.findElement(By.name('customer.firstName')).clear();
+driver.findElement(By.name('customer.firstName')).sendKeys(config.firstName);
+driver.findElement(By.name('customer.lastName')).clear();
+driver.findElement(By.name('customer.lastName')).sendKeys(config.lastName);
+driver.findElement(By.name('customer.dayPhone.npa')).sendKeys(config.areaCode);
+driver.findElement(By.name('customer.dayPhone.nxx')).sendKeys(config.officeCode);
+driver.findElement(By.name('customer.dayPhone.did')).sendKeys(config.lineNumber);
+driver.findElement(By.name('customer.email')).clear();
+driver.findElement(By.name('customer.email')).sendKeys(config.email);
+driver.findElement(By.name('customer.email2')).clear();
+driver.findElement(By.name('customer.email2')).sendKeys(config.email);
+driver.findElement(By.css("option[value='CC']")).click();
+driver.wait(until.elementLocated(By.css("option[value='" + config.cardType + "']")));
+driver.findElement(By.css("option[value='" + config.cardType + "']")).click();
+driver.findElement(By.name('paymentMethod.accountNumber')).sendKeys(config.ccAccount);
+driver.findElement(By.name('paymentMethod.cvv')).sendKeys(config.cvv);
+driver.findElement(By.css("select[name='paymentMethod.creditCardExpiryDate.month'] option[value='" + config.expirationMM + "']")).click();
+driver.findElement(By.css("select[name='paymentMethod.creditCardExpiryDate.year'] option[value='" + config.expirationYYYY + "']")).click();
+driver.findElement(By.name('paymentMethod.cardHolderName')).clear();
+driver.findElement(By.name('paymentMethod.cardHolderName')).sendKeys(config.firstName + " " + config.lastName);
+driver.findElement(By.name('submitBtn')).click();
+driver.findElement(By.name('header.acceptTermsAndConditions')).click();
+driver.findElement(By.name('submitBtn')).click();
+//driver.quit();
+
+/*
+var tableRows = driver.findElements(By.css("tr"));
+var confirmationPage = "";
+
+for(var i = 5; i < tableRows.size; i++) {
+    confirmationPage = confirmationPage + tableRows[i].innerHTML;
+}
+
+fs.writeFile(config.filePathForConfirmation + fileName, confirmationPage, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
+*/
---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "loginUrl": "https://secure.csidfl.org/ols/login.php",
+    "firstName": "John",
+    "lastName": "Doe",
+    "email": "you@email.com",
+    "password": "",
+    "accountNumber": "",
+    "zipCode": "",
+    "areaCode": 555,
+    "officeCode": 867,
+    "lineNumber": 5309,
+    "cardType": "VISA",
+    "ccAccount": "",
+    "cvv": "",
+    "expirationMM": "04",
+    "expirationYYYY": "2017",
+    "filePathForConfirmation": ""
+};
